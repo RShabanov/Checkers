8da7a8df7166f303d4df2d17acdb8b9ace345a2b
Shabanov Roman <shabanov.roman0@gmail.com>
2021-11-13 14:05:44 +0800

    Add input mode in main.cpp and move input files (now they are in input/)

--- draughts/main.cpp
+++ draughts/main.cpp
@@ -4,24 +4,37 @@
 using namespace std;
 
 int main() {
-	// TODO: add file via CLI args
-	Checkers game("input.txt");
-	
-	char firstColor;
+	try {
+		string filename;
+		cout << "Enter input filename: ";
+		cin >> filename;
 
-	while (true) {
-		cout << "First move (B or W): _\b";
-		cin >> firstColor;
-		firstColor = toupper(firstColor);
+		Checkers game(filename);
 
-		if (strchr("BW", firstColor)) break;
+		char firstColor;
 
-		system("cls");
-		cout << "Please, pay attention! Let's try again" << endl;;
+		while (true) {
+			cout << "First move (B or W): _\b";
+			cin >> firstColor;
+			firstColor = toupper(firstColor);
+
+			if (strchr("BW", firstColor)) break;
+
+			system("cls");
+			cout << "Please, pay attention! Let's try again" << endl;;
+		}
+
+		game.run(firstColor == 'W' ? Color::WHITE : Color::BLACK);
+
+		size_t lastindex = filename.find_last_of(".");
+		filename = filename.substr(0, lastindex) +"-output.txt";
+		game.save(filename);
+
+		cout << "Result was saved. File: " << filename << endl;
 	}
-
-	game.run(firstColor == 'W' ? Color::WHITE : Color::BLACK);
-	game.save();
+	catch (exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 
 	return 0;
 }
